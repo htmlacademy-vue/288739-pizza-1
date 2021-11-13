d17622258079680735d40bd2cb38082f0c6ec442
Alex Kapin <alexander.kapin@outlook.com>
2021-11-13 12:19:02 +0300

    Добавлены тесты helpers, AppNotifications, ItemCounter

---
+++ src/frontend/src/store/mocks/index.js
@@ -0,0 +1,21 @@
+import { cloneDeep } from "lodash";
+import { mutations } from "@/store";
+import modules from "@/store/modules";
+import Vuex from "vuex";
+import VuexPlugins from "@/plugins/vuexPlugins";
+
+export const generateMockStore = (actions) => {
+  const modulesCopy = cloneDeep(modules);
+
+  if (actions) {
+    Object.entries(actions).forEach(([module, actions]) => {
+      modulesCopy[module] = { ...modulesCopy[module], actions };
+    });
+  }
+
+  return new Vuex.Store({
+    mutations,
+    modules: modulesCopy,
+    plugins: [VuexPlugins],
+  });
+};
